677499ea810dce6854d9ec8e759abcece731a504
huanghao <huanghao@youxin.cloud>
2024-01-19 21:59:24 +0800

    fix lint

--- src/components/AMISRenderer.vue
+++ src/components/AMISRenderer.vue
@@ -7,31 +7,38 @@ import 'amis/sdk/helper.css';
 import { useRouter } from 'vue-router';
 
 const props = defineProps({
+	/** The amis JSON schema */
 	schema: {
 		type: Object,
 		required: true,
 	},
+	/** 赋予 amis 顶层数据域的值 */
 	locals: {
 		type: Object,
 		default: () => ({}),
 	},
+	/** 控制 amis 的属性，一般都用不上 */
 	props: {
 		type: Object,
 		default: () => ({}),
 	},
+	/** 环境变量，用于控制 amis 的行为，需要使用 amis 用户实现部分接口 */
 	env: {
 		type: Object,
 		default: () => ({}),
 	},
 });
 
+/** The amis component is ready */
 const emit = defineEmits(['ready']);
 
 let amisInstance: any;
 
 onMounted(() => {
 	const scoped = window.amisRequire('amis/embed');
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { normalizeLink } = window.amisRequire('amis');
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const router = useRouter();
 	const instance = scoped.embed(
 		'#amis-component',
