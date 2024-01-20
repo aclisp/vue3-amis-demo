b23daa53d9c2c433a7603ca85599f74242266371
huanghao <huanghao@youxin.cloud>
2024-01-20 18:07:55 +0800

    add el-menu

--- components.d.ts
+++ components.d.ts
@@ -11,8 +11,12 @@ declare module 'vue' {
     ElAside: typeof import('element-plus/es')['ElAside']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/views/MainFrame.vue
+++ src/views/MainFrame.vue
@@ -1,12 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
 
 <template>
 	<el-container class="layout-container">
 		<el-aside width="200px">
-			Pages
-			<div><router-link to="/">Home</router-link></div>
-			<div><router-link to="/about">About</router-link></div>
-			<div><router-link to="/new">New</router-link></div>
+			<div class="brand">
+				<span class="title">VUE3 AMIS DEMO</span>
+			</div>
+			<el-scrollbar>
+				<el-menu :router="true" :default-active="route.path">
+					<el-menu-item index="/home">
+						<el-icon><Menu /></el-icon>
+						<span>Home</span>
+					</el-menu-item>
+					<el-menu-item index="/about">
+						<el-icon><Document /></el-icon>
+						<span>About</span>
+					</el-menu-item>
+					<el-menu-item index="/new">
+						<el-icon><Setting /></el-icon>
+						<span>New</span>
+					</el-menu-item>
+				</el-menu>
+			</el-scrollbar>
 		</el-aside>
 		<el-main>
 			<router-view />
@@ -22,5 +41,15 @@
 .el-aside {
 	background-color: #f0f2f5;
 	line-height: 1.5;
+}
+
+.brand {
+	background-color: #409eff;
+	color: white;
+	height: 56px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-style: italic;
 }
 </style>
