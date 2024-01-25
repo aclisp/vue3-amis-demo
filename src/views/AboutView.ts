81fa9ebf22145641cd90f38213ee613587cc6ce2
huanghao <huanghao@youxin.cloud>
2024-01-26 03:00:54 +0800

    完成增删改查

--- src/views/AboutView.ts
+++ src/views/AboutView.ts
@@ -1,8 +1,97 @@
+import { DIRECTUS_URL } from '@/constants';
 import { attachURL } from '@/utils/file-id-to-url';
+
+const categorySource = {
+	method: 'get',
+	url: '${DIRECTUS_URL}/items/app02_category',
+	data: {
+		fields: ['id', 'name'],
+		parent: '${parentId}',
+	},
+	requestAdaptor: (api: any) => {
+		if (!api.url.startsWith(DIRECTUS_URL)) {
+			api.url = DIRECTUS_URL + api.url;
+		}
+		const { parent } = api.query;
+		if (parent) {
+			api.data = { filter: { parent: { _eq: parent } } };
+		} else {
+			api.data = { filter: { parent: { _null: true } } };
+		}
+	},
+	adaptor: (payload: any) => {
+		return {
+			data: {
+				items: payload.data.items.map((x: any) => ({ label: x.name, value: x.id })),
+			},
+		};
+	},
+};
 
 export default {
 	type: 'page',
 	title: '会员管理',
+	toolbar: [
+		{
+			type: 'button',
+			label: '新增',
+			icon: 'fa fa-plus',
+			level: 'primary',
+			actionType: 'dialog',
+			dialog: {
+				title: '新增记录',
+				body: {
+					type: 'form',
+					rules: [
+						{
+							rule: 'data.category != ""',
+							message: '不能为空',
+							name: 'category',
+						},
+					],
+					api: {
+						method: 'post',
+						url: '${DIRECTUS_URL}/items/app02_product',
+						data: {
+							category: '${ARRAYFILTER(category, item => item) | last}',
+							name: '${name}',
+							current_price: '${current_price}',
+						},
+					},
+					body: [
+						{
+							type: 'chained-select',
+							name: 'category',
+							label: '品类',
+							clearable: true,
+							joinValues: false,
+							extractValue: true,
+							required: true,
+							source: categorySource,
+						},
+						{
+							type: 'input-text',
+							name: 'name',
+							label: '名称',
+							required: true,
+						},
+						{
+							type: 'input-number',
+							name: 'current_price',
+							label: '当前价格',
+							precision: 2,
+							required: true,
+						},
+						{
+							type: 'input-image',
+							name: 'imageURL',
+							label: '图片上传',
+						},
+					],
+				},
+			},
+		},
+	],
 	body: {
 		type: 'crud',
 		api: {
@@ -10,6 +99,12 @@ export default {
 			url: '${DIRECTUS_URL}/items/app02_product',
 			data: {
 				fields: ['id', 'name', 'current_price', 'status', 'image', 'category.name', 'category.id'],
+				filter: {
+					status: {
+						_neq: 'archived',
+					},
+				},
+				sort: ['-date_created'],
 				page: '${page}',
 				limit: '${perPage}',
 				meta: 'filter_count',
@@ -60,17 +155,48 @@ export default {
 									method: 'get',
 									url: '${DIRECTUS_URL}/items/app02_product/${id}',
 									data: {
-										fields: ['id', 'name', 'current_price', 'status', 'image', 'category.name', 'category.id'],
+										fields: [
+											'id',
+											'name',
+											'current_price',
+											'status',
+											'image',
+											'category.name',
+											'category.id',
+											'category.parent.id',
+											'category.parent.parent.id',
+										],
 									},
 									adaptor: (payload: any) => {
 										attachURL(payload.data, ['image']);
+										const { category } = payload.data;
+										payload.data.category = [category.id];
+										if (category.parent?.id) {
+											payload.data.category.unshift(category.parent.id);
+										}
+										if (category.parent?.parent?.id) {
+											payload.data.category.unshift(category.parent.parent.id);
+										}
 										return payload;
 									},
 								},
 								api: {
 									method: 'patch',
 									url: '${DIRECTUS_URL}/items/app02_product/${id}',
+									data: {
+										category: '${ARRAYFILTER(category, item => item) | last}',
+										name: '${name}',
+										current_price: '${current_price}',
+										status: '${status}',
+									},
 								},
+								rules: [
+									{
+										rule: 'data.category != ""',
+										message: '不能为空',
+										name: 'category',
+									},
+								],
 								body: [
 									{
 										type: 'input-text',
@@ -79,21 +205,27 @@ export default {
 										static: true,
 									},
 									{
-										type: 'input-text',
-										name: 'category.name',
+										type: 'chained-select',
+										name: 'category',
 										label: '品类',
-										static: true,
+										clearable: true,
+										joinValues: false,
+										extractValue: true,
+										required: true,
+										source: categorySource,
 									},
 									{
 										type: 'input-text',
 										name: 'name',
 										label: '名称',
+										required: true,
 									},
 									{
 										type: 'input-number',
 										name: 'current_price',
 										label: '当前价格',
 										precision: 2,
+										required: true,
 									},
 									{
 										type: 'input-image',
@@ -104,7 +236,8 @@ export default {
 										type: 'select',
 										name: 'status',
 										label: '状态',
-										options: ['published', 'draft', 'archived'],
+										options: ['published', 'draft'],
+										required: true,
 									},
 								],
 							},
@@ -128,6 +261,15 @@ export default {
 						type: 'button',
 						level: 'link',
 						className: 'text-danger',
+						confirmText: '确认要删除吗？',
+						actionType: 'ajax',
+						api: {
+							method: 'patch',
+							url: '${DIRECTUS_URL}/items/app02_product/${id}',
+							data: {
+								status: 'archived',
+							},
+						},
 					},
 				],
 			},
